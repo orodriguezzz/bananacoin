7bdb6568869fd2888148f719f5ac85d41862735f
Orlando <Orlando@DESKTOP-AHERBC6>
2018-05-18 17:44:38 -0400

    método que valida el SHA256

--- BananacoindotNet.BL/business/business.cs
+++ BananacoindotNet.BL/business/business.cs
@@ -8,6 +8,22 @@ namespace BananacoindotNet.BL.business
 {
     public static class business
     {
-        
+        public static string getSHA256(string password)
+        {
+            string result = string.Empty;
+            try
+            {
+
+                byte[] passwordBytes = Encoding.Unicode.GetBytes(password);
+                var hasher = System.Security.Cryptography.SHA256.Create();
+                byte[] hashedBytes = hasher.ComputeHash(passwordBytes);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                //Save the logs!
+                return result;
+            }
+        }
     }
 }
--- BananacoindotNet/Controllers/cuentasController.cs
+++ BananacoindotNet/Controllers/cuentasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BananacoindotNet.BL.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,14 +12,17 @@ namespace BananacoindotNet.Controllers
         // GET: cuentas
         public ActionResult Index(string direccion)
         {
-            //1: busco y/o creo cuenta
-            var model = BananacoindotNet.BL.data.data.getCreateCuenta(direccion);
-            //2: busco transacciones
-            model.transacciones = BananacoindotNet.BL.data.data.getTransacciones(direccion);
-            //3: obtengo tipo transacciones
-            model.tipoTransacciones = BananacoindotNet.BL.data.data.getTipoTransaccion();
+            //if (BananacoindotNet.BL.business.business.validaSHA256(direccion))
+            //{
+                //1: busco y/o creo cuenta
+                var model = BananacoindotNet.BL.data.data.getCreateCuenta(direccion);
+                //2: busco transacciones
+                model.transacciones = BananacoindotNet.BL.data.data.getTransacciones(direccion);
+                //3: obtengo tipo transacciones
+                model.tipoTransacciones = BananacoindotNet.BL.data.data.getTipoTransaccion();
+            //}
 
-            return View(model);
+            return View(new BC_CuentaSaldoViewModel());
         }
     }
 }
