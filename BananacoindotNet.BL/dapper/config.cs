3d86218de2ea0daa142efedfb5696b842a608c1b
Orlando <Orlando@DESKTOP-AHERBC6>
2018-05-18 16:30:13 -0400

    add BL project

---
+++ BananacoindotNet.BL/dapper/config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BananacoindotNet.BL.dapper
+{
+    public static class config
+    {
+        public static readonly string connectionString = ConfigurationManager.ConnectionStrings["providerConnection"].ConnectionString;
+
+        public static DbConnection GetOpenConnection()
+        {
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ BananacoindotNet.BL/data/data.cs
@@ -0,0 +1,41 @@
+﻿using BananacoindotNet.BL.dapper;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BananacoindotNet.BL.data
+{
+    public static class data
+    {
+        /// <summary>
+        /// Dummy method
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static object getdata(string id)
+        {
+            //result
+            try
+            {
+                using (var con = config.GetOpenConnection())
+                {
+                    con.Query<object>("spBananacoin_get_data",
+                        new { id = id },
+                        commandType: CommandType.StoredProcedure
+                    ).ToList();
+                    con.Close();
+                }
+                return null;
+            }
+            catch (Exception e)
+            {
+                //Save the logs!
+                return null;
+            }
+        }
+    }
+}
